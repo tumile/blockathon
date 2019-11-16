3a234da1ad5ae9470721f8bf750e39c03bf550eb
Tuan Le <tuanle@Tuans-MacBook-Pro.local>
2019-11-16 16:30:05 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -27,13 +27,14 @@ class App extends Component {
 
   runExample = async () => {
     const { account, contract } = this.state;
-    console.log(contract);
 
-    // await contract.addCompany(
-    //   'Google',
-    //   'https://banner2.cleanpng.com/20180324/iww/kisspng-google-logo-g-suite-google-5ab6f1cee66464.5739288415219388949437.jpg',
-    //   { from: account }
-    // );
+    const response = await contract.addCompany(
+      'Google',
+      'https://banner2.cleanpng.com/20180324/iww/kisspng-google-logo-g-suite-google-5ab6f1cee66464.5739288415219388949437.jpg',
+      { from: account }
+    );
+
+    console.log(response);
   };
 
   render() {
--- truffle-config.js
+++ truffle-config.js
@@ -6,8 +6,18 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*',
-      gas: 6721975
+      network_id: '*'
+    }
+  },
+  compilers: {
+    solc: {
+      version: '^0.5.0',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
     }
   }
 };
