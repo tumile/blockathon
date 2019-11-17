ba6e27348f2bcf19f607fe06327d923ccfa9d9a4
Patrick Liem <patrickliem00@gmail.com>
2019-11-16 21:36:05 -0500

    fixed merge issues

--- client/src/App.js
+++ client/src/App.js
@@ -12,20 +12,6 @@ import EmployeePage from "./EmployeePage"
 class App extends Component {
     state = { account: null, contract: null }
 
-<<<<<<< HEAD
-    componentDidMount = async () => {
-        try {
-            const web3 = await getWeb3()
-            const instance = TruffleContract(HireBlockContract)
-            instance.setProvider(web3.currentProvider)
-            const contract = await instance.deployed()
-            const accounts = await web3.eth.getAccounts()
-            const account = accounts[0]
-            this.setState({ account, contract })
-        } catch (err) {
-            console.error(err)
-        }
-=======
   componentDidMount = async () => {
     try {
       const web3 = await getWeb3();
@@ -44,8 +30,8 @@ class App extends Component {
 
     } catch (err) {
       console.error(err);
->>>>>>> d32d7769f9c3cb055a1add0ae15bcd246296563e
     }
+  }
 
     render() {
         if (!this.state.contract) {
@@ -87,40 +73,6 @@ class App extends Component {
             </Router>
         )
     }
-<<<<<<< HEAD
-=======
-    return (
-      <Router>
-        <div>
-          <div className="nav-container">
-            <Menu mode="horizontal">
-              <Menu.Item key="search">
-                <Link to="/">
-                  <Icon type="file-search" />
-                  Lookup
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="manage">
-                <Link to="/manage">
-                  <Icon type="appstore" />
-                  Manage
-                </Link>
-              </Menu.Item>
-            </Menu>
-          </div>
-          <Switch>
-            <Route exact path="/">
-              <LookupPage contract={this.state.contract} />
-            </Route>
-            <Route path="/manage">
-              <ManagePage contract={this.state.contract} />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
->>>>>>> d32d7769f9c3cb055a1add0ae15bcd246296563e
 }
 
 export default App
