31b02d18ed424dae2bf0d96f2e270bc70cdb6cd8
Tuan Le <tuanle@Tuans-MacBook-Pro.local>
2019-11-16 19:54:55 -0500

    dev

--- contracts/HireBlock.sol
+++ contracts/HireBlock.sol
@@ -16,6 +16,7 @@ contract HireBlock {
     }
 
     struct PerformanceReview {
+        address companyID;
         uint timestamp;
         string review;
     }
@@ -23,10 +24,10 @@ contract HireBlock {
     uint nonce = 0;
 
     mapping(address => Company) private companies;
+    mapping(address => Employee) private employees;
 
-    mapping(address => Employee[]) private companyEmployees;
-
-    mapping(address => Employee[]) private companyCandidates;
+    mapping(address => address[]) private companyEmployees;
+    mapping(address => address[]) private companyCandidates;
 
     mapping(address => PerformanceReview[]) private employeeReviews;
 
@@ -36,112 +37,57 @@ contract HireBlock {
         owner = msg.sender;
     }
 
-    function addCompany(string memory companyName, string memory companyImage) public returns (address) {
+    function addCompany(string memory companyName, string memory companyImage) public returns (address companyID) {
         require(msg.sender == owner, "Sender must be owner of contract");
-
-        address companyID = getUniqueId();
+        companyID = getUniqueId();
         Company memory newCompany = Company(companyID, companyName, companyImage);
         companies[companyID] = newCompany;
-
-        return companyID;
     }
 
-    function addEmployee(address companyID, string memory employeeName, string memory employeeImage) public returns (address) {
-        require(companyID < companyList.length, "Company ID is invalid");
-
-        uint employeeAmount = employeeLists[companyID].length;
-
-        Employee memory newEmployee = Employee(employeeAmount, employeeName, employeeImage);
-
-        employeeLists[companyID].push(newEmployee);
-
-        return employeeAmount;
-    }
-
-    function addPerformanceReview(uint companyID, uint employeeID, string memory review, uint timestamp) public {
-        require(companyID < companyList.length, "Company ID is invalid");
-        require(employeeID < employeeLists[companyID].length, "Employee ID is invalid");
-
-        PerformanceReview memory newReview = PerformanceReview(timestamp, review);
-
-        string memory compEmpID = strConcat(uintToString(companyID), "-", uintToString(employeeID));
-
-        employeeReviewsForCompany[compEmpID].push(newReview);
-    }
-
-    function addCandidate(uint companyID, uint employeeID, string memory employeeName, string memory employeeImage) public {
+    function addEmployee(address companyID, string memory employeeName, string memory employeeImage) public returns (address employeeID) {
+        employeeID = getUniqueId();
         Employee memory newEmployee = Employee(employeeID, employeeName, employeeImage);
-        
-        companyCandidates[companyID].push(newEmployee);
+        employees[employeeID] = newEmployee;
+        companyEmployees[companyID].push(employeeID);
     }
 
-    function getEmployeeList(uint companyID) public view returns(Employee[] memory employeeList) {
-        return employeeLists[companyID];
+    function addCandidate(address companyID, address employeeID) public {
+        companyCandidates[companyID].push(employeeID);
     }
 
-     function getPerformanceReviews(uint companyID, uint employeeID) public view returns (PerformanceReview[] memory reviewList) {
-        string memory compEmpID = strConcat(uintToString(companyID), "-", uintToString(employeeID));
-        return employeeReviewsForCompany[compEmpID];
-    }
-
-    function getCompanyCount() public view returns(uint count) {
-        return companyList.length;
-    }
-
-    function getCompany(uint companyID) public view returns(Company memory comp) {
-        return companyList[companyID];
-    }
-
-    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e)
-    internal pure returns (string memory){
-        bytes memory _ba = bytes(_a);
-        bytes memory _bb = bytes(_b);
-        bytes memory _bc = bytes(_c);
-        bytes memory _bd = bytes(_d);
-        bytes memory _be = bytes(_e);
-        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-        bytes memory babcde = bytes(abcde);
-        uint k = 0;
-        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-        for (uint i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-        for (uint i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-        for (uint i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-        for (uint i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-        return string(babcde);
-    }
-
-    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d) internal pure returns (string memory) {
-        return strConcat(_a, _b, _c, _d, "");
-    }
-
-    function strConcat(string memory _a, string memory _b, string memory _c) internal pure returns (string memory) {
-        return strConcat(_a, _b, _c, "", "");
-    }
-
-    function strConcat(string memory _a, string memory _b) internal pure returns (string memory) {
-        return strConcat(_a, _b, "", "", "");
-    }
-
-    function uintToString(uint v) internal pure returns (string memory str) {
-        uint maxlength = 100;
-        bytes memory reversed = new bytes(maxlength);
-        uint i = 0;
-        uint temp = v;
-        while (temp != 0) {
-            uint remainder = temp % 10;
-            temp = temp / 10;
-            reversed[i++] = byte(uint8(48 + remainder));
+    function getEmployeeList(address companyID) public view returns (Employee[] memory employeeList) {
+        address[] memory addresses = companyEmployees[companyID];
+        uint length = addresses.length;
+        employeeList = new Employee[](length);
+        for (uint i = 0; i < length; i++) {
+            employeeList[i] = employees[addresses[i]];
         }
-        bytes memory s = new bytes(i + 1);
-        for (uint j = 0; j <= i; j++) {
-            s[j] = reversed[i - j];
-        }
-        str = string(s);
     }
 
-    function getUniqueId() internal returns (address) {
-        address random = address(uint160(uint(keccak256(abi.encodePacked(nonce, blockhash(block.number))))));
+    function getCandidateList(address companyID) public view returns (Employee[] memory employeeList) {
+        address[] memory addresses = companyCandidates[companyID];
+        uint length = addresses.length;
+        employeeList = new Employee[](length);
+        for (uint i = 0; i < length; i++) {
+            employeeList[i] = employees[addresses[i]];
+        }
+    }
+
+    function addPerformanceReview(address companyID, address employeeID, string memory review, uint timestamp) public {
+        PerformanceReview memory newReview = PerformanceReview(companyID, timestamp, review);
+        employeeReviews[employeeID].push(newReview);
+    }
+
+    function getPerformanceReviews(address employeeID) public view returns (PerformanceReview[] memory performanceReviews) {
+        performanceReviews = employeeReviews[employeeID];
+    }
+
+    function getCompany(address companyID) public view returns(Company memory company) {
+        company = companies[companyID];
+    }
+
+    function getUniqueId() internal returns (address random) {
+        random = address(uint160(uint(keccak256(abi.encodePacked(nonce, blockhash(block.number))))));
         nonce++;
-        return address(random);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,60 @@
 var HireBlock = artifacts.require('./HireBlock.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(HireBlock).then(function(instance) {
-    instance.addCompany("Google", "https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip");
-    instance.addCompany("Microsoft", "https://cdn.pixabay.com/photo/2013/02/12/09/07/microsoft-80660_960_720.png");
-
-    instance.addEmployee(0, "John Doe", "https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png");
-    instance.addEmployee(0, "Alice Smith", "https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png");
-    instance.addEmployee(0, "Dana Washington", "https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png");
-    instance.addEmployee(1, "Bob Jones", "https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png");
-    instance.addEmployee(1, "James Lincoln", "https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png");
-
-    instance.addPerformanceReview(0, 0, "Good job this year", Date.UTC(2017, 12, 28));
-    instance.addPerformanceReview(0, 0, "Another good year", Date.UTC(2018, 12, 28));
-
-    instance.addPerformanceReview(0, 1, "Good job this year", Date.UTC(2017, 12, 26));
-
+  deployer.deploy(HireBlock).then(async function(instance) {
+    const google = await instance.addCompany(
+      'Google',
+      'https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg'
+    );
+    const micro = await instance.addCompany(
+      'Microsoft',
+      'https://cdn.pixabay.com/photo/2013/02/12/09/07/microsoft-80660_960_720.png'
+    );
+    const john = await instance.addEmployee(
+      google,
+      'John Doe',
+      'https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png'
+    );
+    const alice = await instance.addEmployee(
+      micro,
+      'Alice Smith',
+      'https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png'
+    );
+    const dana = await instance.addEmployee(
+      google,
+      'Dana Washington',
+      'https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png'
+    );
+    const bob = await instance.addEmployee(
+      micro,
+      'Bob Jones',
+      'https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png'
+    );
+    const james = await instance.addEmployee(
+      micro,
+      'James Lincoln',
+      'https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png'
+    );
+    instance.addPerformanceReview(
+      google,
+      john,
+      'Good job this year',
+      Date.UTC(2017, 12, 28)
+    );
+    instance.addPerformanceReview(
+      google,
+      dana,
+      'Another good year',
+      Date.UTC(2018, 12, 28)
+    );
+    instance.addPerformanceReview(
+      micro,
+      james,
+      'Excellent performance',
+      Date.UTC(2017, 12, 26)
+    );
+    instance.addCandidate(google, alice);
+    instance.addCandidate(google, james);
+    instance.addCandidate(google, bob);
   });
 };
